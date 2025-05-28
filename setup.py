1cafe0fd8275417b33d206815f622ab6731b7aab
ebpowell <ebpowell.chip@gmail.com>
2025-05-28 00:11:17 -0400

    Dockerfile now builds correctly. Still doesn't run...

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='ww_door_controller',
     version='0.1',
-    # packages=['door_controller/access_control_list.py',
-    #           'door_controller/data_extractor.py',
-    #           'door_controller/database',
-    #           'door_controller/door_controller',
-    #           'door_controller/fobs',
-    #           'door_controller/pg_database',
-    #           'door_controller/swipes'],
     packages=find_packages(),
     scripts=['scripts/get_recent_swipes.py'],
     url='',
