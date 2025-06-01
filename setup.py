19a432315c7dda977a3979d56d39d823fdfc61de
Eric Powell <30306348+ebpowell@users.noreply.github.com>
2025-05-31 21:21:28 -0400

    Add files via upload
    Initial import to GitHb

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='ww_door_controller',
+    version='0.1',
+    packages=find_packages(),
+    # List external dependencies from requirements.txt
+    # install_requires=install_requires,
+
+    # Define your command-line entry points (console scripts)
+    entry_points={
+        'console_scripts': [
+            'get_swipes=door_controller.tools.get_recent_swipes:main',
+            'get_acl_from_door_controller=door_controller.tools.get_acl_from_controller:main',
+            'update_controller=door_controller.tools.update_controller:main',
+            # ... add more tools here
+        ]
+    },
+    python_requires='>=3.8',
+   # tools=['tools/get_recent_swipes.py'],
+    url='',
+    license='',
+    author='ebpowell',
+    author_email='ebpowell.chip@gmail.com',
+    description=''
+)
